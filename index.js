18ea8f1c5347e348b4469147555d297b5ff38d3c
SpencerBlum <spencerblum@gmail.com>
2020-03-28 16:04:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here
+
+let var1 = 2
+let var2 = 31
+
+const multiply = var1 * var2
+
+const random = Math.floor(Math.random(3)) + 1 
+
+let mod = 29 % 5
+
+const max = Math.max (1, 2, 0, 15, 20)
+
+
+
